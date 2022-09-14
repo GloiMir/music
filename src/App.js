cd0c2177d7ca1c66721ec6534d9930aeae13d4bf
GloiMir <gloirmirindi@gmail.com>
2022-09-14 16:20:14 +0200

    Feat:logout system,environment variable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import './App.css'
+
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, } from 'react-google-login';
+import { gapi } from 'gapi-script';
 import Recherche from './components/Recherche';
 
-
-
 function App() {
+  const [search, setSearch] = useState(false)
+  const clientId = process.env.REACT_APP_CLIENT_ID_GOOGLE;
 
-  return (
-    <Recherche />
-  );
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ''
+      });
+    };
+    gapi.load('client:auth2', initClient);
+  });
+
+  const onSuccess = (res) => {
+    setSearch(true)
+  };
+
+  const onFailure = (err) => {
+    console.log('failed', err);
+  };
+  console.log(process.env.REACT_APP_CLIENT_ID_GOOGLE+"Rien")
+  const Login = () => {
+    return (
+      <div className='login'>
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Sign in with Google"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={'single_host_origin'}
+          isSignedIn={false}
+        />
+      </div>
+    )
+  }
+  if (!search) {
+    return (
+      <Login />
+    );
+  } else {
+      return (
+        <div>
+          <Recherche />
+          <button className='logout' onClick={() => setSearch(false)}>Se deconnecter</button>
+        </div>
+      )
+    }
 }
-
 export default App;
--- src/components/Recherche.js
+++ src/components/Recherche.js
@@ -1,107 +1,17 @@
-// import React, { useState, useRef,useEffect } from 'react'
-
-// const CLIENT_ID = "1a89139441e04dbba687217471298e3c"
-// const CLIENT_SECRET = "8d2ccff09951406ba467592c4d5d1117"
-// export default function Recherche() {
-//     useEffect(()=>{
-//         let params = {
-//             method:'POST',
-//             headers:{
-//                 'Content-Type':'application/x-www-form-urlencode'
-//             },
-//             body:'grant_type=client_credentials&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET
-//         }
-//         fetch('https://accounts.spotify.com/api/token',params)
-//         .then(res=>res.json())
-//         .then(res=>console.log(res.access_token))
-//     },[])
-
-//     const [titre, setTitre] = useState('');
-//     const [select, setSelect] = useState([])
-//     const [choose, setChoose] = useState(false)
-
-
-//     let songs = [
-//         {
-//             'title': 'Je suis là',
-//             'auteur': 'Moise MBIYE',
-//             'year': 2014,
-//             'song': require('./suis.mp3')
-//         },
-//         {
-//             'title': 'Je suis là encore',
-//             'auteur': 'Moise MBIYE',
-//             'year': 1996,
-//             'song': require('./suis.mp3')
-//         },
-//         {
-//             'title': 'Royal',
-//             'auteur': 'Moise MBIYE',
-//             'year': 2016,
-//             'song': require('./suis.mp3')
-//         },
-//         {
-//             'title': 'Royal encore',
-//             'auteur': 'Moise Mbiye',
-//             'year': 2005,
-//             'song': require('./suis.mp3')
-//         },
-//         {
-//             'title': 'Royal et je suis là',
-//             'auteur': 'Moise MBIYE',
-//             'year': 1995,
-//             'song': require('./suis.mp3')
-//         },
-//         {
-//             'title': 'Je suis là et royal',
-//             'auteur': 'Moise MBIYE',
-//             'year': 1995,
-//             'song': require('./suis.mp3')
-//         }
-//     ];
-
-//     let music;
-
-//     const filtreChanson = (element) => {
-//         if (element.title === titre) {
-//             return element
-//         }
-//     }
-//     let ref1 = useRef(null)
-//     let ref2 = useRef(null)
-//     return (
-//         <div className='principale'>
-//             <input placeholder='Entrez le titre'
-//                 onChange={(e) => { setTitre(e.target.value) }}
-//             ></input>
-//             <button
-//                 onClick={() => { if (music != null) { music.pause(); setSelect(songs.filter(filtreChanson)); setChoose(true); } else { setSelect(songs.filter(filtreChanson)); setChoose(true); } }}
-//             >Chercher</button>
-//             {
-//                 choose && <span onClick={() => { music = new Audio(select[0].song); music.play(); console.log('On est en train de lire cette chanson') }}>{select[0].title}</span>
-//             }
-//             <div>
-//                 <button ref={ref1} onClick={() => { music.play(); }}>Play</button>
-//                 <button ref={ref2} onClick={() => { music.pause(); }}>Pause</button>
-//             </div>
-//         </div>
-//     )
-// }
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { TabContainer, InputGroup, FormControl, Button, Card, Row } from 'react-bootstrap'
 import { useState, useEffect } from 'react'
 
-const CLIENT_ID = "1a89139441e04dbba687217471298e3c"
-const CLIENT_SECRET = "8d2ccff09951406ba467592c4d5d1117"
-
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID_SPOTIFY
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
 
 export default function Recherche() {
-    const [searchInput, setSearchInput] = useState("")
+    const [searchInput, setSearchInput] = useState("Moise MBIYE")
     const [accessToken, setAccessToken] = useState("")
     const [artistId, setArtisteId] = useState('')
     const [albums, setAlbums] = useState([]);
-    const [albumId,setAlbumId] = useState('7AFT8fGI07iOK064OqxItM')
-    const [showIframe,setShowIframe] = useState(false)
+    const [albumId,setAlbumId] = useState('2HkiLlN9SXxPYlKM7FxbSn')
+    const [showIframe,setShowIframe] = useState(true);
 
     useEffect(() => {
         //API access token
@@ -117,6 +27,7 @@ export default function Recherche() {
             .then(data => setAccessToken(data.access_token))
     }, [])
 
+    //Search albums list by artist name
     async function search() {
         let searchParameters = {
             method: 'GET',
@@ -128,39 +39,21 @@ export default function Recherche() {
         fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
             .then(response => response.json())
             .then(data => setArtisteId(data.artists.items[0].id))
-    }
-    useEffect(() => {
-        let searchParameters = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + accessToken
-            }
-        }
+
         fetch('https://api.spotify.com/v1/artists/' + artistId + '/albums?include_groups=album&market=US&limit=50', searchParameters)
             .then(response => response.json())
-            .then(data => setAlbums(data.items))
-        console.log(albums)
-    }, [accessToken, artistId, albums])
+            .then(data =>{setAlbums(data.items);})
+    }
+    search();
 
     return (
-        <div>
-            <TabContainer className='mb-3' size='1g'>
-                <InputGroup>
-                    <FormControl
-                        placeholder='Search for artist'
-                        type='input'
-                        onChange={(event) =>{setSearchInput(event.target.value);}}
-                    />
-                    <Button onClick={search}>Search</Button>
-                </InputGroup>
-            </TabContainer>
-            <TabContainer>
+        <div className='search'>
+            <div className='albums'>
                 <Row className='mx-2 row row-cols-4'>
                     {
                         albums && albums.map((item, index) => {
                             return (
-                                <Card>
+                                <Card className='carte'>
                                     <Card.Img src={item.images[0].url} onClick={()=>{setAlbumId(item.id); setShowIframe(true)}} />
                                     <Card.Body>
                                         <Card.Title>{item.name}</Card.Title>
@@ -170,12 +63,22 @@ export default function Recherche() {
                         })
                     }
                     {                
-                        showIframe &&  <iframe  src={"https://open.spotify.com/embed/album/"+albumId+"?utm_source=generator"} width="100%" height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> 
+                        showIframe &&  <iframe title='my reader' className='lecteur'  src={"https://open.spotify.com/embed/album/"+albumId+"?utm_source=generator"} width="100%" height="100%" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> 
                     } 
-
                 </Row>
-
-            </TabContainer>
+            </div>
+            <div>
+                <TabContainer className='mb-3' size='1g'>
+                    <InputGroup className='inputsearch'>
+                        <FormControl
+                            placeholder="Entrez le nom de l'artiste"
+                            type='input'
+                            onChange={(event) =>{setSearchInput(event.target.value);}}
+                        />
+                        <Button onClick={search}>Chercher</Button>
+                    </InputGroup> 
+                </TabContainer> 
+            </div>
         </div>
     )
 }
